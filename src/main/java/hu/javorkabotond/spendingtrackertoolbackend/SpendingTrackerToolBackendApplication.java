1789d0025d502838fa0b0a797e4756816bb68f70
Botond Javorka <javorka.botond@gmail.com>
2022-11-15 13:52:58 +0100

    feat: Create base backend

---
+++ src/main/java/hu/javorkabotond/spendingtrackertoolbackend/SpendingTrackerToolBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.javorkabotond.spendingtrackertoolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingTrackerToolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpendingTrackerToolBackendApplication.class, args);
+    }
+
+}
